fbac443da3d982dd704987cd953163532af9ae48
sandjonyves <sandjonyves@gmail.com>
2025-06-14 01:23:26 +0200

    add integration test in jobs

--- backend/tests/middleware/authorize.middleware.test.js
+++ backend/tests/middleware/authorize.middleware.test.js
@@ -2,14 +2,18 @@ const authorize = require('../../middleware/authorize.middleware');
 
 describe('🧪 CT-UM-04 – Accès refusé si rôle non autorisé (authorize)', () => {
   it('renvoie 403 si le rôle utilisateur n’est pas autorisé', () => {
-    const req = { role: 'user' }; // rôle actuel
+    const req = {
+      headers: {
+        'x-user-role': 'user' // rôle actuel
+      }
+    };
     const res = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn()
     };
     const next = jest.fn();
 
-    const middleware = authorize('admin'); // seul "admin" autorisé
+    const middleware = authorize(['admin']); // seul "admin" est autorisé
     middleware(req, res, next);
 
     expect(res.status).toHaveBeenCalledWith(403);
@@ -17,21 +21,25 @@ describe('🧪 CT-UM-04 – Accès refusé si rôle non autorisé (authorize)', 
     expect(next).not.toHaveBeenCalled();
   });
 
-  describe(' CT-UM-05 – Accès autorisé si rôle permis (authorize)', () => {
-  it('appelle next() si le rôle utilisateur est autorisé', () => {
-    const req = { role: 'admin' }; // rôle actuel
-    const res = {
-      status: jest.fn(),
-      json: jest.fn()
-    };
-    const next = jest.fn();
+  describe('🧪 CT-UM-05 – Accès autorisé si rôle permis (authorize)', () => {
+    it('appelle next() si le rôle utilisateur est autorisé', () => {
+      const req = {
+        headers: {
+          'x-user-role': 'admin' // rôle actuel
+        }
+      };
+      const res = {
+        status: jest.fn(),
+        json: jest.fn()
+      };
+      const next = jest.fn();
 
-    const middleware = authorize('admin'); // "admin" autorisé
-    middleware(req, res, next);
+      const middleware = authorize(['admin']); // "admin" est autorisé
+      middleware(req, res, next);
 
-    expect(next).toHaveBeenCalled();            // accès autorisé
-    expect(res.status).not.toHaveBeenCalled();  // aucune erreur
-    expect(res.json).not.toHaveBeenCalled();
+      expect(next).toHaveBeenCalled();            // accès autorisé
+      expect(res.status).not.toHaveBeenCalled();  // aucune erreur
+      expect(res.json).not.toHaveBeenCalled();
+    });
   });
-});
 });
