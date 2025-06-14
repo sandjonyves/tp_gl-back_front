1c103cec3e349bbe6f21a4cc99067a6c41acc4f7
sandjonyves <sandjonyves@gmail.com>
2025-06-14 10:49:29 +0200

    deploiement frontend

--- frontend/src/components/dashboard/ProjectHeader.tsx
+++ frontend/src/components/dashboard/ProjectHeader.tsx
@@ -4,7 +4,7 @@ export const ProjectHeader = () => {
   return (
     <div className="text-center mb-16">
       <h1 className="text-4xl font-bold bg-gradient-to-r from-pink-500 to-purple-600 bg-clip-text text-transparent mb-4">
-        TP INF35 GENIE LOGICIEL
+        TP INF352 GENIE LOGICIEL
       </h1>
       <h2 className="text-2xl text-gray-600">
         Les Testes Logiciel
